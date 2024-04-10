df26538593fd02ad622c4d9d85fc923980949105
HaiBooLang <53350622+HaiBooLang@users.noreply.github.com>
2024-04-10 14:55:58 +0800

    19.1 值与对象

--- CSalmon/src/value.h
+++ CSalmon/src/value.h
@@ -9,6 +9,7 @@ typedef enum {
 	VAL_BOOL,
 	VAL_NIL,
 	VAL_NUMBER,
+	VAL_OBJ,	// 每个状态位于堆上的Lox值都是一个Obj。
 } ValueType;
 
 // 这个类型定义抽象了Salmon值在C语言中的具体表示方式。这样，我们就可以直接改变表示方法，而不需要回去修改现有的传递值的代码。
@@ -17,28 +18,36 @@ typedef enum {
 // 在本地编译器的机器码中，这些较大的常量会存储在二进制可执行文件中的一个单独的“常量数据”区域。
 // 然后，加载常量的指令会有一个地址和偏移量，指向该值在区域中存储的位置。
 // 每个字节码块都会携带一个在程序中以字面量形式出现的值的列表。为简单起见，我们会把所有的常量都放进去，甚至包括简单的整数。
+// 
 // 有一个字段用作类型标签，然后是第二个字段，一个包含所有底层值的联合体。
 // 大多数体系结构都喜欢将值与它们的字长对齐。
 // 由于联合体字段中包含一个8字节的double值，所以编译器在类型字段后添加了4个字节的填充，以使该double值保持在最近的8字节边界上。
+// 
+// 对于小的、固定大小的类型（如数字），有效载荷直接存储在Value结构本身。
+// 如果对象比较大，它的数据就驻留在堆中。那么Value的有效载荷就是指向那块内存的一个指针。
 typedef struct {
 	ValueType type;
 	union {
 		bool boolean;
 		double number;
+		Obj* obj;
 	} as;
 } Value;
 
 // 其中每个宏都接收一个适当类型的C值，并生成一个Value，其具有正确类型标签并包含底层的值。这就把静态类型的值提升到了clox的动态类型的世界。
 #define BOOL_VAL(value)   (Value{VAL_BOOL, {.boolean = value}})
 #define NIL_VAL           (Value{VAL_NIL, {.number = 0}})
+#define OBJ_VAL(object)   (Value{VAL_OBJ, {.obj = (Obj*)object}})
 #define NUMBER_VAL(value) (Value{VAL_NUMBER, {.number = value}})
 // 但是为了能对Value做任何操作，我们需要将其拆包并取出对应的C值。
 #define AS_BOOL(value)    ((value).as.boolean)
+#define AS_OBJ(value)     ((value).as.obj)
 #define AS_NUMBER(value)  ((value).as.number)
 // 除非我们知道Value包含适当的类型，否则使用任何的AS_宏都是不安全的。为此，我们定义最后几个宏来检查Value的类型。
 // 如果Value具有对应类型，这些宏会返回true。每当我们调用一个AS_宏时，我们都需要保证首先调用了这些宏。
 #define IS_BOOL(value)    ((value).type == VAL_BOOL)
 #define IS_NIL(value)     ((value).type == VAL_NIL)
+#define IS_OBJ(value)     ((value).type == VAL_OBJ)
 #define IS_NUMBER(value)  ((value).type == VAL_NUMBER)
 
 // ------------------常量池------------------
